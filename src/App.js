f28cee4e6e6ba96803f60dc872c143e275c84970
Patrik Näsström <patrik_nasstrom@hotmail.com>
2020-04-19 14:39:54 +0200

    added venuNfo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './resources/styles.css'
 
 import Header from './components/header_footer/Header'
 import Featured from './components/featured/index'
+import VenueNfo from './components/venueNfo/index'
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height:"1500px", background:'cornflowerblue'}}>
         <Header />
         <Featured/>
+        <VenueNfo/>
       </div>
     )
   }
